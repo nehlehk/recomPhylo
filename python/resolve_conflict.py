194139b80a461b8fafbbd69628d5167efaac5e47
kargarfard870@gmail.com <F@temeh870>
2021-01-06 13:43:59 +1100

    test input

--- python/resolve_conflict.py
+++ python/resolve_conflict.py
@@ -29,10 +29,9 @@ taxa = dendropy.TaxonNamespace(taxon_list)
 
 
 clonal_tree = '(((4:0.001239110283681363,1:0.001239110283681363):0.0018547139845565177,(0:0.0012213226202040935,5:0.0012213226202040935):0.0018725016480337874):0.006906175731762117,(((3:0.0006986298786672515,9:0.0006986298786672515):0.0029400514371517444,6:0.0036386813158189955):0.0024892092060997675,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05):0.0019138765529503985):0.004195315253768946):0.003872109478081235):0.0;'
-recomtrees = ['((((4:0.001239110283681363,1:0.001239110283681363)10:0.0018547139845565177,(0:0.0012213226202040935,5:0.0012213226202040935)11:0.0018725016480337874)12:0.006906175731762117,((3:0.003638681315818996,6:0.0036386813158189955)14:0.0024892092060997675,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05)15:0.0019138765529503985)16:0.004195315253768946)17:0.003872109478081235)18:0.08566688291907873,9:0.09566688291907872);\n',
- '((((4:0.001239110283681363,1:0.001239110283681363)10:0.0018547139845565177,(0:0.0012213226202040935,5:0.0012213226202040935)11:0.0018725016480337874)12:0.006906175731762117,((3:0.0006986298786672515,9:0.0006986298786672515)13:0.0029400514371517444,6:0.0036386813158189955)14:0.006361318684181003)18:0.09609486245186069,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05)15:0.0019138765529503985)16:0.10416228718371087);\n',
- '((((4:0.001239110283681363,1:0.001239110283681363)10:0.0018547139845565177,(0:0.0012213226202040935,5:0.0012213226202040935)11:0.0018725016480337874)12:0.006906175731762117,(6:0.006127890521918763,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05)15:0.0019138765529503985)16:0.004195315253768946)17:0.003872109478081235)18:0.09213752399306009,(3:0.0006986298786672515,9:0.0006986298786672515)13:0.10143889411439283);\n']
-recomnodes = [9,16,13]
+recomtrees = ['((((4:0.001239110283681363,1:0.001239110283681363)10:0.0018547139845565177,0:0.003093824268237881)12:0.006906175731762117,(((3:0.0006986298786672515,9:0.0006986298786672515)13:0.0029400514371517444,6:0.0036386813158189955)14:0.0024892092060997675,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05)15:0.0019138765529503985)16:0.004195315253768946)17:0.003872109478081235)18:0.08750634189181826,5:0.09750634189181825);\n',
+ '(((4:0.001239110283681363,1:0.001239110283681363)10:0.008760889716318634,(((3:0.0006986298786672515,9:0.0006986298786672515)13:0.0029400514371517444,6:0.0036386813158189955)14:0.0024892092060997675,(8:0.0019325752681498172,(2:1.86987151994185e-05,7:1.86987151994185e-05)15:0.0019138765529503985)16:0.004195315253768946)17:0.003872109478081235)18:0.09474906003601989,(0:0.0012213226202040935,5:0.0012213226202040935)11:0.1035277374158158);\n']
+recomnodes = [5,11]
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -144,8 +143,8 @@ def resolve_conflict(clonaltree, recomtrees, recomnodes):
 
 
 
-                    print(maintree.as_string(schema="newick"))
-                    print(maintree.as_ascii_plot())
+                    # print(maintree.as_string(schema="newick"))
+                    # print(maintree.as_ascii_plot())
 
 
 
@@ -312,6 +311,76 @@ def give_older_recom(s_equ):
   return [a for a in s_equ if (count[a[0]] == 1) or (a[2]==max)]
 
 # ----------------------------------------------------------------------------------------------------------------------
+def recom_maker(tree ,node ,recom_length):
+    new_tree = dendropy.Tree(taxon_namespace=taxa)
+    parent = node.parent_node
+    # changeing in topology when recombiantion is larger than tree.max_distance_from_root()
+    if recom_length > tree.max_distance_from_root():
+      print("------------------------ recom_length > tree.max_distance_from_root() ------------------------------")
+      if (node.edge_length is None):
+        node.edge.length = 0
+      # new_tree = dendropy.Tree(taxon_namespace=taxa)
+      if node.is_leaf():
+        external_len = recom_length + node.edge_length - tree.max_distance_from_root()
+        tree.prune_subtree(node)
+        other_nodes = tree.seed_node
+        new_tree.seed_node.add_child(other_nodes)
+        new_tree.seed_node.add_child(node)
+        other_nodes.edge_length = external_len
+        node.edge_length = recom_length + node.edge_length
+      else:
+        tree.prune_subtree(node)
+        other_nodes = tree.seed_node
+        new_tree.seed_node.add_child(other_nodes)
+        new_tree.seed_node.add_child(node)
+        node.edge_length = recom_length + node.edge_length
+        other_nodes.edge_length = node.distance_from_tip() + node.edge_length - other_nodes.distance_from_tip()
+    #*************************************************************************
+    # topology does not change in this case:
+    elif ((recom_length + node.edge_length) < parent.distance_from_tip()) and (node.is_leaf):
+        print(" *********** ((recom_length + node.edge_length) < parent.distance_from_tip()) and (node.is_leaf)  ***********")
+        node.edge.length = node.edge.length + recom_length
+        sister = node.sister_nodes()
+        sister[0].edge.length = sister[0].edge.length + recom_length
+        parent.edge.length = parent.edge.length - recom_length
+        new_tree = tree
+    elif ((recom_length + node.edge_length) < parent.distance_from_tip()) and (node.is_internal):
+        print(" *********** ((recom_length + node.edge_length) < parent.distance_from_tip()) and (node.is_internal)  ***********")
+        node.edge.length = node.edge.length + recom_length
+        new_tree = tree
+    #*************************************************************************
+    # changing in topology to make recombination tree:
+    elif ((recom_length + node.edge_length) > parent.distance_from_tip())  and ((recom_length + node.edge_length) < tree.max_distance_from_root()):
+        print("((recom_length + node.edge_length) > parent.distance_from_tip())  and ((recom_length + node.edge_length) < tree.max_distance_from_root())")
+        ancestor = []
+        recom_length = recom_length + node.edge_length
+        for id,tmp_node in enumerate(node.ancestor_iter()):
+            ancestor.append(tmp_node)
+            # print(id ,"::::::",tmp_node.index)
+            if recom_length < tmp_node.distance_from_tip() :
+                attached_node = tmp_node
+                attached_id = id
+                # print(attached_node.index)
+                break
+
+        relocated_nodes = ancestor[attached_id-1]  # relocated node is the adjacent node of recombinant node
+        parent.remove_child(node)         # the original recombinant node was removed to reinsert in the other side
+        attached_node.remove_child(relocated_nodes) # relocated node was removed to reinsert in to right side
+        newborn = dendropy.datamodel.treemodel.Node()  # newborn in the new mrca of recombinant node and its sister
+        newborn.edge_length = attached_node.distance_from_tip() - recom_length
+        node.edge_length = recom_length
+        newborn.add_child(node)
+        relocated_nodes.edge_length = relocated_nodes.edge_length - newborn.edge_length
+        newborn.add_child(relocated_nodes)
+        attached_node.add_child(newborn)
+        new_tree = tree
+
+    #*************************************************************************
+
+    return new_tree
+
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -320,4 +389,27 @@ set_index(initialTree)
 
 merge_Recomtrees(recomtrees,recomnodes)
 
-resolve_conflict(initialTree,recomtrees,recomnodes)
+unconfilcted_str = resolve_conflict(initialTree,recomtrees,recomnodes)
+print(unconfilcted_str)
+
+# unconfilcted_tree = Tree.get_from_string(unconfilcted_str, schema='newick')
+# set_index(unconfilcted_tree)
+#
+# tree = Tree.get_from_string(clonal_tree, schema='newick')
+# set_index(tree)
+#
+# res = set()
+# desc = give_descendents(tree, 16, res)
+# print(desc)
+# new_node = my_mrca(unconfilcted_tree, list(desc))
+# print(new_node)
+#
+# res2 = set()
+# desc2 = give_descendents(unconfilcted_tree, new_node, res2)
+# print(desc2)
+#
+# mynode = unconfilcted_tree.find_node_with_label(str(new_node))
+# result = recom_maker(unconfilcted_tree,mynode,0.10416229)
+#
+# print(result.as_string(schema="newick"))
+# print(result.as_ascii_plot())
