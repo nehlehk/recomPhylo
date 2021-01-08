027f7652d6cdb6ab431a47b17612bc4d0ac48309
kargarfard870@gmail.com <F@temeh870>
2021-01-08 15:09:28 +1100

    Command Line Arguments

---
+++ pythonProject/RecomPhylo/phyloHMM.py
@@ -0,0 +1,335 @@
+import numpy as np
+import numpy.linalg as la
+import pandas as pd
+import matplotlib.pyplot as plt
+import hmmlearn.base
+import logging
+import hmmlearn._utils
+from scipy.integrate import quad
+from dendropy import Tree, DnaCharacterMatrix
+import dendropy
+from hmmlearn import hmm
+import string
+import xml.etree.ElementTree as ET
+from dendropy.calculate import treecompare
+from sklearn.metrics import mean_squared_error
+import math
+import sys
+
+
+
+class GTR_model:
+    def __init__(self, rates, pi):
+        self.rates = rates
+        self.pi = pi
+    #     ========================================================================
+    def get_pi(self):
+        return self.pi
+    #     ========================================================================
+    def p_matrix(self , br_length):
+        p = np.zeros((4, 4))
+
+        mu = 0
+        freq = np.zeros((4, 4))
+        q = np.zeros((4, 4))
+        sqrtPi = np.zeros((4, 4))
+        sqrtPiInv = np.zeros((4, 4))
+        exchang = np.zeros((4, 4))
+        s = np.zeros((4, 4))
+        fun = np.zeros(1)
+        a, b, c, d, e = self.rates
+        f = 1
+
+        freq = np.diag(self.pi)
+        sqrtPi = np.diag(np.sqrt(self.pi))
+        sqrtPiInv = np.diag(1.0 / np.sqrt(self.pi))
+        mu = 1 / (2 * ((a * self.pi[0] * self.pi[1]) + (b * self.pi[0] * self.pi[2]) + (c * self.pi[0] * self.pi[3]) + (d * self.pi[1] * self.pi[2]) + (
+                e * self.pi[1] * self.pi[3]) + (self.pi[2] * self.pi[3])))
+        exchang[0][1] = exchang[1][0] = a
+        exchang[0][2] = exchang[2][0] = b
+        exchang[0][3] = exchang[3][0] = c
+        exchang[1][2] = exchang[2][1] = d
+        exchang[1][3] = exchang[3][1] = e
+        exchang[2][3] = exchang[3][2] = f
+
+
+        q = np.multiply(np.dot(exchang, freq), mu)
+
+        for i in range(4):
+            q[i][i] = -sum(q[i][0:4])
+
+
+        s = np.dot(sqrtPi, np.dot(q, sqrtPiInv))
+
+
+        eigval, eigvec = la.eig(s)
+        eigvec_inv = la.inv(eigvec)
+
+        left = np.dot(sqrtPiInv, eigvec)
+        right = np.dot(eigvec_inv, sqrtPi)
+
+        p = np.dot(left, np.dot(np.diag(np.exp(eigval * br_length)), right))
+
+
+        return p
+# **********************************************************************************************************************
+class phyloLL_HMM(hmmlearn.base._BaseHMM):
+    def __init__(self, n_components, trees, model ,child_order,recom_child_order):
+        super().__init__(n_components)
+        self.trees = trees
+        self.model = model
+        self.child_order = child_order
+        self.recom_child_order = recom_child_order
+
+    def _init(self, X, lengths):
+
+        """Initializes model parameters prior to fitting.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Feature matrix of individual samples.
+            n_samples == number of alignment sites
+            n_features == 12: 4 site partials for each of 3 neighbour nodes
+
+        lengths : array-like of integers, shape (n_sequences, )
+            Lengths of the individual sequences in ``X``. The sum of
+            these should be ``n_samples``.
+        """
+        init = 1. / self.n_components
+        if 's' in self.init_params or not hasattr(self, "startprob_"):
+            self.startprob_ = np.full(self.n_components, init)
+        if 't' in self.init_params or not hasattr(self, "transmat_"):
+            self.transmat_ = np.full((self.n_components, self.n_components), init)
+        n_fit_scalars_per_param = self._get_n_fit_scalars_per_param()
+        n_fit_scalars = sum(n_fit_scalars_per_param[p] for p in self.params)
+        if X.size < n_fit_scalars:
+            _log.warning("Fitting a model with {} free scalar parameters with "
+                         "only {} data points will result in a degenerate "
+                         "solution.".format(n_fit_scalars, X.size))
+
+    #     ==========================================================================
+    def _check(self):
+        """Validates model parameters prior to fitting.
+
+        Raises
+        ------
+
+        ValueError
+            If any of the parameters are invalid, e.g. if :attr:`startprob_`
+            don't sum to 1.
+        """
+        self.startprob_ = np.asarray(self.startprob_)
+        if len(self.startprob_) != self.n_components:
+            raise ValueError("startprob_ must have length n_components")
+        if not np.allclose(self.startprob_.sum(), 1.0):
+            raise ValueError("startprob_ must sum to 1.0 (got {:.4f})"
+                             .format(self.startprob_.sum()))
+
+        self.transmat_ = np.asarray(self.transmat_)
+        if self.transmat_.shape != (self.n_components, self.n_components):
+            raise ValueError(
+                "transmat_ must have shape (n_components, n_components)")
+        if not np.allclose(self.transmat_.sum(axis=1), 1.0):
+            raise ValueError("rows of transmat_ must sum to 1.0 (got {})"
+                             .format(self.transmat_.sum(axis=1)))
+
+    #     ==========================================================================
+    def _compute_log_likelihood(self, X):
+        """Computes per-component log probability under the model.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Feature matrix of individual samples.
+
+        Returns
+        -------
+            Log probability of each sample in ``X`` for each of the
+            model states.
+        """
+
+        return compute_logprob_phylo(X, self.trees, self.model, self.child_order, self.recom_child_order)
+
+# **********************************************************************************************************************
+def give_index(c):
+    if c == "A":
+        return 0
+    elif c == "C":
+        return 1
+    elif c == "G":
+        return 2
+    elif c == "T":
+        return 3
+# **********************************************************************************************************************
+def set_index(tree, dna):
+    sequence_count = len(dna)
+
+    for node in tree.postorder_node_iter():
+        node.index = -1
+        node.annotations.add_bound_attribute("index")
+
+    s = sequence_count
+    for node in tree.postorder_node_iter():
+        if not node.is_leaf():
+            node.index = s
+            node.label = str(node.index)
+            s += 1
+        else:
+            for idx, name in enumerate(dna):
+                if str(name) == str(node.taxon):
+                    node.index = idx
+                    node.label = str(node.index)
+                    break
+# **********************************************************************************************************************
+def get_DNA_fromAlignment(alignment):
+
+    alignment_len = alignment.sequence_size
+    tips = len(alignment)
+    column = []
+    for l in range(alignment_len):
+        col = ""
+        for t in range(tips):
+            col += str(alignment[t][l])
+        column.append(col)
+
+    return column
+# **********************************************************************************************************************
+def set_tips_partial(tree, alignment):
+    partial = np.zeros(((alignment_len, tips_num, 4)))
+    for tip in range(tips_num):
+      for site in range(alignment_len):
+        dna = column[site]
+        i = give_index(str(dna[tip]))
+        partial[site][tip][i] = 1
+    return partial
+# **********************************************************************************************************************
+def computelikelihood_mixture(tree, alignment ,tip_partial, model):
+    alignment_len = alignment.sequence_size
+    tips = len(dna)
+    partial = np.zeros(((alignment_len,(2 * tips) -1, 4)))
+    partial[:,0:tips,:] = tip_partial
+    persite_ll = np.zeros(alignment_len)
+
+
+    column = get_DNA_fromAlignment(alignment)
+
+    uniqueCol = list(set(column))
+    for u in range(len(uniqueCol)):
+      indexes = []
+      indexes = [id for id, x in enumerate(column) if x == uniqueCol[u]]
+      site = indexes[0]
+      for node in tree.postorder_node_iter():
+          if not node.is_leaf():
+              children = node.child_nodes()
+              partial[site,node.index] = np.dot(model.p_matrix(children[0].edge_length), partial[site,children[0].index])
+              for i in range(1, len(children)):
+                  partial[site, node.index] *= np.dot(model.p_matrix(children[i].edge_length),partial[site,children[i].index])
+      partial[indexes,:,:] = partial[site,:,:]
+      p = np.dot(partial[site,tree.seed_node.index] , model.get_pi())
+      persite_ll[indexes] = np.log(p)
+
+
+    return persite_ll, partial
+# **********************************************************************************************************************
+def make_hmm_input_mixture(tree, alignment, tip_partial, model):
+    sitell, partial = computelikelihood_mixture(tree, alignment, tip_partial, model)
+    children = tree.seed_node.child_nodes()
+    children_count = len(children)
+    x = np.zeros((alignment.sequence_size, children_count * 4))
+    for id, child in enumerate(children):
+        # print(child.index)
+        x[:, (id * 4):((id + 1) * 4)] = partial[:, child.index, :]
+    return x
+# **********************************************************************************************************************
+def update_mixture_partial(alignment,tree,node,tipdata,posterior,node_order):
+  column = get_DNA_fromAlignment(alignment)
+
+  for site in range(alignment_len):
+    dna = column[site]
+    my_number = give_index(dna[node.index])
+    rho = give_rho(node,posterior,site,tips_num,node_order)
+    for i in range(4):
+      if i == my_number:
+        tipdata[site,node.index,i] = 1
+      else:
+        tipdata[site,node.index,i] = rho
+
+  return tipdata
+# **********************************************************************************************************************
+def give_rho(node,recom_prob,site,tips_num,node_order):
+  parent = node.parent_node
+  if parent == tree.seed_node:
+    myindex = parent.index -1
+  else:
+    myindex = parent.index
+  # node_prob = recom_prob[myindex - tips_num][site]
+  # node_prob = recom_prob[site]
+  # rho = 1 - node_prob[0]
+  rho = recom_prob[site][node_order]
+  return rho
+# **********************************************************************************************************************
+def tree_evolver_rerooted(tree ,node ,nu):
+    co_recom = nu/2
+    if (node.edge_length is None):
+       node.edge.length = 0
+    node.edge.length = node.edge.length + co_recom
+    recombination_tree = tree.as_string(schema="newick")
+
+    return recombination_tree
+# **********************************************************************************************************************
+def give_taxon(tree,index):
+  for node in tree.postorder_node_iter():
+    if node.index == index:
+        return str(node.taxon)
+# **********************************************************************************************************************
+def compute_logprob_phylo_fast(X, recom_trees, model):
+    n, dim = X.shape
+    result = np.zeros((n, len(recom_trees)))
+    for tree_id, item in enumerate(recom_trees):
+        state_tree = dendropy.Tree.get(data=item, schema="newick")
+        children = state_tree.seed_node.child_nodes()
+
+        uniqueX = np.unique(X, axis=0)
+        for uniq_id, partial in enumerate(uniqueX):
+            indexes = []
+            indexes = [id for id, x in enumerate(X) if (x == uniqueX[uniq_id]).all()]
+            p = np.zeros(4)
+            p = np.dot(model.p_matrix(children[0].edge_length), partial[0:4])
+            for i in range(1, len(children)):
+                p *= np.dot(model.p_matrix(children[i].edge_length), partial[i * 4:(i + 1) * 4])
+            site_l = np.dot(p, model.get_pi())
+            result[indexes, tree_id] = np.log(site_l)
+    return result
+# **********************************************************************************************************************
+def compute_logprob_phylo(X, recom_trees, model,child_order,recom_child_order):
+    n, dim = X.shape
+    result = np.zeros((n, len(recom_trees)))
+    for tree_id, item in enumerate(recom_trees):
+        state_tree = dendropy.Tree.get(data=item, schema="newick")
+        children = state_tree.seed_node.child_nodes()
+        for site_id, partial in enumerate(X):
+            order = child_order.index(recom_child_order[tree_id * len(children)])
+            p = np.zeros(4)
+            p = np.dot(model.p_matrix(children[0].edge_length), partial[order * 4:(order + 1) * 4])
+            for i in range(1, len(children)):
+                order = child_order.index(recom_child_order[(tree_id* len(children)) + i])
+                p *= np.dot(model.p_matrix(children[i].edge_length), partial[order * 4:(order + 1) * 4])
+            site_l = np.dot(p, model.get_pi())
+            result[site_id, tree_id] = np.log(site_l)
+    return result
+# **********************************************************************************************************************
+
+
+
+if __name__ == "__main__":
+    tree_path = sys.argv[1]
+    genomefile = sys.argv[2]
+
+    tree = Tree.get_from_path(tree_path, 'newick')
+    alignment = dendropy.DnaCharacterMatrix.get(file=open(genomefile), schema="fasta")
+
+    print(tree.as_ascii_plot(show_internal_node_labels=True))
+
+
+
